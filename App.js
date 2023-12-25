0ae44087f79c572348c3861278df5116ecf61037
AishaTalat2001 <aisha.talat123@gmail.com>
2023-12-25 08:06:04 +0500

    Updates

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { View,} from "react-native"
 import MainLayout from './src/layouts/MainLayout'
 
-
-const Stack = createNativeStackNavigator()
-
 const App = () => {
   return (
     <View style={{flex:1, backgroundColor:'white'}}>
